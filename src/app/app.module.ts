d69f094635242f405bf179d0bc9ac07a142393c5
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-09-16 08:37:57 -0400

    installed npm packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MDBBootstrapModule} from "angular-bootstrap-md";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule
   ],
   providers: [],
